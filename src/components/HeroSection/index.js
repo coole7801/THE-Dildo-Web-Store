7b6a5b8dbb3d9c23a6954cf634aeab112f4c4998
coole7801 <chrisconte51@gmail.com>
2022-11-28 18:53:37 -0800

    Overlay Fixed
    falndoiawd

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { HeroContainer, HeroBg, VideoBg, overlay } from './HeroElements'
 
 const HeroSection = () => {
   return (
+    <overlay>
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-        <div id='overlay'></div>
       </HeroBg>
     </HeroContainer>
+    </overlay>
   )
 }
 
