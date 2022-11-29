78fd71a3f216e9b70b1e5e26b1b5f7eff13cf314
coole7801 <chrisconte51@gmail.com>
2022-11-29 14:06:24 -0800

    Cum 2
    thfhnfyj

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,44 +1,28 @@
 import styled from "styled-components";
 
 export const HeroContainer = styled.div`
-    background: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0 30px;
-    height: 800px;
-    position: relative;
-    z-index: 1;
+    width: 100%;
+    height: 100%;
+    text-align: center;
 `;
 
 export const HeroBg = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+    position: relative;
 `;
 
 export const VideoBg = styled.video`
     width: 100%;
     height: 100%;
-    -o-object-fit: cover;
     object-fit: cover;
     backround: black;
 `;
 
-export const overlay = styled.div`
-    position: fixed;
-    display: block;
-    width: 100%;
-    height: 100%;
-    top: 0;
+export const Overlay = styled.div`
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.6);
+    z-index: 2;
     left: 0;
     right: 0;
+    top: 0;
     bottom: 0;
-    background-color: rgba(0,0,0,0.2);
-    z-index: 999; 
-`
+`;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg, overlay } from './HeroElements'
+import Video from '../../videos/video2.mp4';
+import { HeroContainer, HeroBg, VideoBg, Overlay } from './HeroElements'
 
 const HeroSection = () => {
   return (
-    <overlay>
     <HeroContainer>
       <HeroBg>
+      <Overlay />
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
     </HeroContainer>
-    </overlay>
   )
 }
 
