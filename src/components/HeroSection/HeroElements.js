efe1f5e622626e8605bc8c162b7c4e7773d9f854
coole7801 <chrisconte51@gmail.com>
2022-12-01 16:58:32 -0800

    Adding text to homepage
    esdefesfs

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md';
 
 export const HeroContainer = styled.div`
     width: 100%;
@@ -25,4 +26,47 @@ export const Overlay = styled.div`
     right: 0;
     top: 0;
     bottom: 0;
+`;
+
+
+
+export const HeroContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24pxl;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const HeroH1 = styled.h1`
+    color: white;
+    font-size: 48px;
+    text-align: center; 
+`;
+
+export const HeroP = styled.p`
+    margin-top:  24px;
+    color: white;
+    font-size: 24px;
+    text-align: center;
+    max-width: 600px;
+`;
+
+export const HeroBtnWrapper = styled.div`
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ArrowForward = styled(MdArrowForward)`
+    margin-left: 8px;
+    font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+    margin-left: 8px;
+    font-size: 20px;
 `;
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import Video from '../../videos/video2.mp4';
-import { HeroContainer, HeroBg, VideoBg, Overlay } from './HeroElements'
+import {useState} from 'react'
+import { Button } from '../ButtonElement'
+import { HeroContainer, HeroBg, Overlay, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 
 const HeroSection = () => {
+  const [ hover, setHover ] = useState(false)
+
+  const onHover = () => {
+      setHover(!hover)
+  }
+
   return (
     <HeroContainer>
       <HeroBg>
       <Overlay />
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>The Best Dildo Store Around</HeroH1>
+        <HeroP>Our pattened custom build feataure combined with our signature design, guarentees
+          a high quality dildo with very purchase.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started { hover ? <ArrowForward /> : <ArrowRight /> }
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
